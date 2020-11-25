393f1c2e30875665525a046e8c0ec3ea103448e6
IkerSanJuan <isanjuanr1900@alumno.ipn.mx>
2020-11-24 21:41:02 -0600

    Metodos parte 2

--- 2doMetodos/Biblio.java
+++ 2doMetodos/Biblio.java
@@ -9,12 +9,12 @@ public class Biblio{
 
         num = Integer.parseInt(JOptionPane.showInputDialog("\nSi desea retirar alg\u00fan libro, digite el n\u00famero asignado a este."));
 
-        do{
+        if((num  <= 1) || (num > 2)){
             num = Integer.parseInt(JOptionPane.showInputDialog("\nPor favor, digite un t\u00edtulo existente y con existencias."));
-        }while(num != 3);
+        }
 
         JOptionPane.showMessageDialog(null,"\nHa usted extra\u00eddo con \u00e9xito el libro");
-        num = Integer.parseInt(JOptionPane.showInputDialog("\u00bfDesea devolver el libro o extraer otro?\nDigite 1 para devolver o 2 para extraer otro."));
+        num = Integer.parseInt(JOptionPane.showInputDialog("\u00bfDesea devolver el libro o extraer otro?\nDigite 1 para devolver , 2 para extraer otro y 3 para salir"));
 
         if(num == 1){
             JOptionPane.showMessageDialog(null,"\nDevoluci\u00f3n realizada con \u00e9xito.");
@@ -22,7 +22,7 @@ public class Biblio{
         }
         else if(num == 2){
             num = Integer.parseInt(JOptionPane.showInputDialog("\nError. No se cuentan con existencias suficientes para un pr\u00e9stamo."
-                                                                + "\nDevuelva el libro con el que cuenta actualmente digitando 1 o digite 2 para huir."));
+                                                                + "\nDevuelva el libro con el que cuenta actualmente digitando 1 o digite 2."));
                 if(num == 1){
                     menupb.Libros();
                 }
--- 2doMetodos/EstanteP.java
+++ 2doMetodos/EstanteP.java
@@ -1,11 +1,11 @@
 public class EstanteP{
     public void Libros(){
-    Biblio libpres = new Biblio();
-    LibWE WE = new LibWE();
-    LibLDC ladivina = new LibLDC();
+        Biblio libpres = new Biblio();
+        LibWE WE = new LibWE("Wigetta: un viaje magico","Autores: Vegetta777, Willyrex",0,3,"Tiene existencia");
+        LibLDC ladivina = new LibLDC("Divina comedia","Autor: Dante Alighieri",1,0,"Tiene existencia");
 
-    WE.mostrarLibWiVe();
-    ladivina.mostrarLibLaDivCo();
-    libpres.LibPres();
+        WE.mostrarLibWiVe();
+        ladivina.mostrarLibLaDivCo();
+        libpres.LibPres();
     }
 }
